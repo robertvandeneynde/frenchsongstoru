b2f38cc16d6e0672c2d5be705d1e3225c7384fe4
Robert Vanden Eynde <robertvandeneynde@hotmail.com>
2025-04-03 05:56:46 +0300

    Update replace_nasals.py

--- replace_nasals.py
+++ replace_nasals.py
@@ -8,6 +8,9 @@ with open(args.file, 'r', encoding='utf-8') as f:
 
 content = content.replace('~', '\u0303')
 content = content.replace('ё-', 'е\u0304')
+content = content.replace('е-', 'е\u0304')
+content = content.replace('о-', 'о\u0304')
+content = content.replace('_', '\u203F')
 
 with open(args.file, 'w', encoding='utf-8') as f:
 	f.write(content)
