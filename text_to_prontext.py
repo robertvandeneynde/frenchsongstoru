65af7658d2b6afc4818968a3ec97a86f2254d0b2
Robert Vanden Eynde <robertvandeneynde@hotmail.com>
2025-04-03 06:07:21 +0300

    text_to_prontext.py

---
+++ text_to_prontext.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+from glob import glob
+import re
+from pathlib import Path as path
+L = glob('*.txt')
+i = int(input('\n'.join('{}) {}'.format(i, x) for i,x in enumerate(L)) + '\n'))
+filename = L[i]
+s = open(filename, encoding="utf-8").read()
+o = re.sub('(\n\n|\n)', lambda m: {'\n\n': '[]\n\n--\n\n', '\n': '\n[]\n\n'}[m.group(0)], s)
+P = path(outfilename := filename + '.out')
+if P.exists():
+	print(f'Error: file {outfilename!r} exists')
+else:
+	P.write_text(o)
